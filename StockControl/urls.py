b62d3af5c97263632c2687684001981e4f9543bc
“[Juan <[taraguipora2005@gmail.com]>
2024-05-01 12:52:04 -0300

    Correccion Urls

--- StockControl/urls.py
+++ StockControl/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path ('admin/', admin.site.urls),
     path('', views.listar_productos, name='inicio'),  # 
     path('agregar_editar_producto/<int:id>/', views.agregar_editar_producto, name='agregar_editar_producto'),
     path('agregar-proveedor/', views.agregar_proveedor, name='agregar_proveedor'),
